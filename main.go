38c1345b66fb2637e8f295ce6fdb869bca459653
sluggard <sydra@163.com>
2022-01-11 14:28:48 +0800

    增加了命令行选项，增加了start命令

--- main.go
+++ main.go
@@ -11,30 +11,48 @@ import (
 	_ "github.com/sluggard/myfile/docs"
 )
 
-var configFile = flag.String("c", config.DefaultConfigPath, "配置文件路径")
-var version = flag.Bool("v", false, "打印版本号")
+var (
+	configFile = flag.String("c", config.DefaultConfigPath, "设置项目配置文件路径`<path>`，可选")
+	version    = flag.Bool("v", false, "打印版本号")
+	command    string
+)
 
 //Version 程序版本号
-const Version = "0.0.1"
+const Version = "0.0.4"
 
 func main() {
 	log.SetLevel(log.TraceLevel)
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "usage: %s [options] [command]\n", os.Args[0])
 		fmt.Fprintf(os.Stderr, "Commands:\n")
-		fmt.Fprintf(os.Stderr, "\n")
-		fmt.Fprintf(os.Stderr, "  -c <path>\n")
-		fmt.Fprintf(os.Stderr, "    设置项目配置文件路径，可选\n")
-		fmt.Fprintf(os.Stderr, "  -v 打印项目版本号，默认为: false\n")
-		fmt.Fprintf(os.Stderr, "    打印版本号\n")
-		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "  start 启动项目\n")
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+		// fmt.Fprintf(os.Stderr, "  -c <path>\n")
+		// fmt.Fprintf(os.Stderr, "    设置项目配置文件路径，可选\n")
+		// fmt.Fprintf(os.Stderr, "  -v 打印项目版本号，默认为: false\n")
+		// fmt.Fprintf(os.Stderr, "    打印版本号\n")
+		// fmt.Fprintf(os.Stderr, "\n")
 	}
 	flag.Parse()
 
+	if (os.Args[len(os.Args)-1])[0] != '-' {
+		command = os.Args[len(os.Args)-1]
+	}
+
 	if *version {
-		fmt.Println(fmt.Sprintf("版本号：%s\n", Version))
+		fmt.Println(fmt.Sprintf("版本号：%s", Version))
+	}
+
+	switch command {
+	case "start":
+		fmt.Println("server starting...")
+	default:
+		flag.Usage()
 		return
 	}
+
+	// if(os.Args[-1])
 
 	irisServer := application.NewServer(config.LoadConfing(*configFile))
 	log.Info(config.GetConfig())
